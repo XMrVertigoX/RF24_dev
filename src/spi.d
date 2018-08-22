f93970a06140cfd2d8aae610c5fc69aa32366d42
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2018-08-22 03:13:00 +0200

    Switch to libopencm3

---
+++ src/spi.d
@@ -0,0 +1,27 @@
+src/spi.o: src/spi.c lib/libopencm3//include/libopencm3/stm32/rcc.h \
+ lib/libopencm3//include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/8.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/8.2.0/include/stdbool.h \
+ lib/libopencm3//include/libopencm3/stm32/memorymap.h \
+ lib/libopencm3//include/libopencm3/stm32/f3/memorymap.h \
+ lib/libopencm3//include/libopencm3/cm3/memorymap.h \
+ lib/libopencm3//include/libopencm3/stm32/f3/rcc.h \
+ lib/libopencm3//include/libopencm3/stm32/common/rcc_common_all.h \
+ lib/libopencm3//include/libopencm3/stm32/usart.h \
+ lib/libopencm3//include/libopencm3/stm32/f3/usart.h \
+ lib/libopencm3//include/libopencm3/stm32/common/usart_common_all.h \
+ lib/libopencm3//include/libopencm3/stm32/common/usart_common_v2.h \
+ lib/libopencm3//include/libopencm3/stm32/spi.h \
+ lib/libopencm3//include/libopencm3/stm32/f3/spi.h \
+ lib/libopencm3//include/libopencm3/stm32/common/spi_common_v2.h \
+ lib/libopencm3//include/libopencm3/stm32/common/spi_common_all.h \
+ lib/libopencm3//include/libopencm3/stm32/gpio.h \
+ lib/libopencm3//include/libopencm3/stm32/f3/gpio.h \
+ lib/libopencm3//include/libopencm3/stm32/common/gpio_common_f234.h \
+ lib/libopencm3//include/libopencm3/stm32/common/gpio_common_all.h
